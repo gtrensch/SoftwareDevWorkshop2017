4892febb7453ca27140a85b678cb5c3eb7ab50ff
lilianacaldeira <l.caldeira@fz-juelich.de>
2017-11-20 14:12:47 +0100

    wrote factorial in output

--- printNumbers/printNumbers.py
+++ printNumbers/printNumbers.py
@@ -66,4 +66,4 @@ if __name__ == '__main__':
     if params.functionIndex == CONST_FUNC_CODE_FIBONACCI:
         print('fib(' + str(params.operand) + ') =', result)
     elif params.functionIndex == CONST_FUNC_CODE_FACTORIAL:
-        print(str(params.operand) + '! =', str(result))
+        print('factorial ' + str(params.operand) + '! =', str(result))
